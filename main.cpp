8553c5b7b97e97d2627fe679dbbd44fce802875c
Vinicius i5 Tecgraf <viniciusalmada10@gmail.com>
2020-03-11 09:45:07 -0300

    Updated: code style

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <GLFW/glfw3.h>
 
-int main()
-{
-	GLFWwindow* window;
+int main() {
+	GLFWwindow *window;
 
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
@@ -20,15 +18,14 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
+	while (!glfwWindowShouldClose(window)) {
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBegin(GL_TRIANGLES);
 		glVertex2d(-0.5f, -0.5);
-		glVertex2d( 0.0f,  0.5);
-		glVertex2d( 0.5f, -0.5);
+		glVertex2d(+0.0f, +0.5);
+		glVertex2d(+0.5f, -0.5);
 		glEnd();
 
 		/* Swap front and back buffers */
